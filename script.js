964a68010fa6bdcf9dcbfab232cdb8c50c8e7773
Khairy Yusuf Aqlan <harun_za1n@yahoo.com>
2019-06-21 21:23:06 +0700

    test

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-function sortByCharacter (text) {
-  // Your code here
+function sortByCharacter (text) 
+{
+//   var arr = text.split('')
+//   var temp
+//   for ( var i = 0; i < arr.length; i++)
+//   {
+//     for (var j = i + 1; j < arr.length; j++)
+//     {
+//       if(arr[i] > arr[j])
+//       {
+//         temp = arr[i]
+//         arr[i] = arr[j]
+//         arr[j] = temp
+//       }
+//     }
+//   } return arr.join('')
+// }
+
+    var arr = text.split('')
+    var sorting = arr.sort()
+    return sorting.join('')
 }
 
 // TEST CASES
